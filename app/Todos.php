0cf9d7a2ace7ea94b49bf1c6dd6a27983c3e10af
= <algha2013@gmail.com>
2019-11-02 19:05:37 +0200

    upload files

---
+++ app/Todos.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+
+class Todos extends Model
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'title','decription','completed',
+    ];
+
+
+
+
+}
